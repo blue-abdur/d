f394aeca1af2aee8ee0927df3e6301706daaa32f
blue-abdur <93152521+blue-abdur@users.noreply.github.com>
2021-11-04 16:52:13 +0100

    Update Script devoir 2.R

--- Script devoir 2.R
+++ Script devoir 2.R
@@ -12,7 +12,7 @@ HDRS <- read_excel("outils hdrs.xls")
 groupe <- read_excel("outils groupe.xls")
 
 
-## Construction de la base de données pour l'analyse de survie
+## Construction de la base de donnÃ©es pour l'analyse de survie
 
 # Construction de la variable HAMD16
 HDRS17 <- HDRS
@@ -22,39 +22,29 @@ HDRS17 <- subset(HDRS17, select = -c(HAMD16A, HAMD16B))
 HDRS17$score <- apply(HDRS17[3:19], 1, sum)
 HDRStot <- HDRS17[,c(1,2,20)]
 
-# Constuction de la table score à chaque visite
+# Constuction de la table score Ã  chaque visite
 HDRSlarge <- spread(HDRStot, VISIT,score)
 HDRSlarge <- HDRSlarge[,c("NUMERO","J0","J4","J7","J14","J21","J28","J42","J56")]
-
-# /problème à J7 128 : pas de score mais attribution
-
-
 
 
 #construction de la table survie
 
 HDRSsurv <- HDRSlarge                                     # duplication de la table
 HDRSsurv <- HDRSsurv %>% as.data.frame()                  # formattage pour faciliter la manipulation 
-seuil <- matrix(rep(HDRSsurv[,2]/2,8), ncol=8)            # Création d'une matrice contenant sur chaque ligne le 
-HDRSsurv[,2:9] <- HDRSsurv[,2:9]-seuil                    # seuil clinique pour valider un évènement, soustraction à
-                                                          # la matrice initiale: si resultat négatif, evenement 
-event <- HDRSsurv[,2:9]                                   # atteint. Vecteur contenant les individus ayant déclanché
+seuil <- matrix(rep(HDRSsurv[,2]/2,8), ncol=8)            # CrÃ©ation d'une matrice contenant sur chaque ligne le 
+HDRSsurv[,2:9] <- HDRSsurv[,2:9]-seuil                    # seuil clinique pour valider un Ã©vÃ¨nement, soustraction Ã 
+                                                          # la matrice initiale: si resultat nÃ©gatif, evenement 
+event <- HDRSsurv[,2:9]                                   # atteint. Vecteur contenant les individus ayant dÃ©clanchÃ©
 event <- ifelse(event<=0, 1, 0)                           # l'evenement. 
 succ <- apply(event,1,max,na.rm=TRUE)                    
 
-temps <- (c(0,4,7,14,21,28,42,56))                        # vecteur contenant les valeurs temps des différentes 
+temps <- (c(0,4,7,14,21,28,42,56))                        # vecteur contenant les valeurs temps des diffÃ©rentes 
                                                           # visites pour les retrouver. 
 tempssucc <- t(apply(event,1, FUN = function(x){x*temps}))
-tempssucc[tempssucc == 0] <- NA                           # Création d'une table avec seulement les temps >0,
-delai <- apply(tempssucc,1,min,na.rm=TRUE)                # récupération des minimums pour les evenement, recueil 
-                                                          # dans délai
+tempssucc[tempssucc == 0] <- NA                           # CrÃ©ation d'une table avec seulement les temps >0,
+delai <- apply(tempssucc,1,min,na.rm=TRUE)                # rÃ©cupÃ©ration des minimums pour les evenement, recueil 
+                                                          # dans dÃ©lai
 
-
-NonNAindex <- which(!is.na(z))
-firstNonNA <- min(NonNAindex)
-
-# set the next 3 observations to NA
-is.na(z) <- seq(firstNonNA, length.out=3)
 
 
 
